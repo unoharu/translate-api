edebc3e898df267480b44cd5be984d3a2e922c29
unoharu <uno.ha07@gmail.com>
2024-10-22 13:50:33 +0900

    [remove] 不要なimport削除

--- translate_api.py
+++ translate_api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 import requests
